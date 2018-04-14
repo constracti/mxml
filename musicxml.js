952add23014bf1f1f03ceb3c6e3cffe4d6d7a5b0
constracti <constracti@gmail.com>
2018-04-14 19:20:24 +0300

    pass clef as a note struct parameter

--- musicxml.js
+++ musicxml.js
@@ -77,30 +77,10 @@ function parseDuration( type ) {
 	}
 }
 
-function parsePitch( pitch, clef ) {
-	let steps = [
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'A',
-		'B',
-	];
-	let codes = {
-		C: 0,
-		D: 1,
-		E: 2,
-		F: 3,
-		G: 4,
-		A: 5,
-		B: 6,
-	}
-	let step = pitch.getElementsByTagName( 'step' )[0].innerHTML.toUpperCase();
-	let octave = parseInt( pitch.getElementsByTagName( 'octave' )[0].innerHTML );
-	let code = 7 * octave + codes[step];
-	code += Vex.Flow.clefProperties( clef ).line_shift * 2;
-	return steps[code % 7] + '/' + Math.trunc(code / 7);
+function parsePitch( pitch ) {
+	var step = pitch.getElementsByTagName( 'step' )[0].innerHTML;
+	var octave = parseInt( pitch.getElementsByTagName( 'octave' )[0].innerHTML );
+	return step + '/' + octave;
 }
 
 /*
@@ -320,6 +300,8 @@ function parseXML( xml ) {
 							note.duration = parseDuration();
 						// dots
 						note.dots = element.getElementsByTagName( 'dot' ).length;
+						// clef
+						note.clef = stave.clef;
 						if ( element.getElementsByTagName( 'rest' ).length ) {
 							if ( note.duration === 'w' )
 								note.keys = [ 'c/5' ];
@@ -329,7 +311,7 @@ function parseXML( xml ) {
 							vf_note = new Vex.Flow.StaveNote( note );
 							// TODO center align whole measure rests
 						} else {
-							note.keys = [ parsePitch( element.getElementsByTagName( 'pitch' )[0], stave.clef ) ];
+							note.keys = [ parsePitch( element.getElementsByTagName( 'pitch' )[0] ) ];
 							// TODO accidentals https://usermanuals.musicxml.com/MusicXML/Content/EL-MusicXML-alter.htm
 							// TODO chords https://usermanuals.musicxml.com/MusicXML/Content/EL-MusicXML-chord.htm
 							vf_note = new Vex.Flow.StaveNote( note );
